af69498dd1f69f80d0b2d1ce74e692252bd93c92
Isaac Huffman <isaacphuffman@gmail.com>
2020-09-27 21:31:00 -0400

    added image transfer

--- Makefile
+++ Makefile
@@ -1,3 +1,21 @@
+# may have to: export DISPLAY=localhost:0
+#
+# required packages:
+#	libglew-dev
+#	libglfw3-dev
+#	libfreetype6-dev
+#
+# to get -lfreetype to work:
+# 	cd /usr/include/
+#	ln -s freetype2/freetype/ freetype
+#	sudo ln -s freetype2/ft2build.h ft2build.h
 
-all:
-	g++ ./src/main.cpp -o ./bin/sc2.exe
+game:
+	g++ -c -o obj/main.o src/main.cpp
+	g++ -c -o obj/resources/import.o src/resources/import.cpp
+	g++ obj/main.o obj/resources/import.o -lGLEW -lglfw3 -lGL -lpthread -lX11 -ldl -o ./bin/sc2
+
+resources:
+	g++ ./src/resources/export.cpp -ldl -lfreetype -lpng -o ./bin/export
+
+all: game resources
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,56 @@
 #include <iostream>
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include "resources/import.h"
 
 int main() {
+
+    glfwSetErrorCallback([](int error, auto message) {
+        std::cerr << "glfw error (" << error << ") " << message << "\n";
+    });
+    if(!glfwInit()) {
+        std::cerr << "glfwInit failed\n";
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Test", NULL, NULL);
+
+    glfwMakeContextCurrent(window);
+
+    glfwSetKeyCallback(window, [](GLFWwindow* win, int key, int scancode, int action, int mods) {
+        if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+            glfwSetWindowShouldClose(win, GLFW_TRUE);
+        }
+    });
+
+    glewExperimental = true;
+    glewInit();
+
+    glClearColor(1, 0, 0, 1);
+
+    Dependencies dependencies = loadDependencies("res.dat");
+    for(int i = 0; i < dependencies.images.size(); ++i) {
+        std::cout << dependencies.images[i].name << ", ";
+        std::cout << dependencies.images[i].width << ", " << dependencies.images[i].height << "\n";
+    }
+
+    while(!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+            glfwSetWindowShouldClose(window, GLFW_TRUE);
+        }
+
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+
     std::cout << "Hello World!" << std::endl;
     return 0;
 }
